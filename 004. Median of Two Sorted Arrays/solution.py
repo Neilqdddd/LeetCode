92d8e3f161238db850e756d49bace169ada060b8
Yili Lin <linyili159632@gmail.com>
2020-07-14 12:07:36 -0500

    Day 2 not solved

---
+++ 004. Median of Two Sorted Arrays/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        pass
+        '''
+        not solved yet
+        '''
+
+
+if __name__ == '__main__':
+    nums1 = [1, 2, 3, 4]
+    nums2 = [3, 4, 5, 6]
+    print(Solution().findMedianSortedArrays(nums1, nums2))
