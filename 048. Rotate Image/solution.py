a8f086b151b66237f22707f53b9c0eecb5462014
Yili Lin <linyili159632@gmail.com>
2020-08-04 17:36:39 -0500

    Day 15

---
+++ 048. Rotate Image/solution.py
@@ -0,0 +1,29 @@
+'''
+终于给老婆搬完家能回家刷题了。。。tough week
+'''
+
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()
+
+
+if __name__ == '__main__':
+    matrix = [
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
+    ]
+    Solution().rotate(matrix)
+    print(matrix)
